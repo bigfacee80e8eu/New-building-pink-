1d0c1b3b080942c128ef2c8b682d987e2ff7e581
bigfacee80e8eu <dotswap844@gmail.com>
2025-06-17 01:36:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const myloader = document.getElementById("preloader");
+
+window.onload = (event) => {
+  myloader.style.display = "none";
+};
+
+const updateDate = () => {
+  const newdate = new Date();
+  let newdateUpdate = newdate.getFullYear();
+  let dateUpdate = document.getElementById("date");
+  dateUpdate.innerHTML = newdateUpdate;
+};
+updateDate();
+
+function fade( ) {
+
+    document.getElementById("")
+}
